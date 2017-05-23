afb5860c393027b77cc0fc5bce28d972e4c1103b
wjliu1998 <wjliu1998@gmail.com>
2017-05-23 08:44:14 +0800

    implement pocket algorithm

--- perceptron/pri_perceptron.cpp
+++ perceptron/pri_perceptron.cpp
@@ -4,38 +4,43 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <stdlib.h>
 
 using namespace std;
 using std::cout;
 using std::endl;
+//get random number
+int random(int floor, int ceiling){
+  return (rand() % (ceiling - floor + 1)) + floor;
+}
 
 void
 Pri_perceptron::getAnswer(){
   cout << "w: ";
-  int size = sizeof(w) / sizeof(w[0]);
-  for(int i = 0; i <= size; i++){
-    cout << *(w+i) <<" ";
+  for(int i = 0; i < size; i++){
+    cout << w[i] <<" ";
   }
   cout << endl;
-
   cout << "b: " << b << endl;
+  cout << "W: ";
+  for(int i = 0; i < size; i++){
+    cout << W[i] <<" ";
+  }
+  cout << endl;
+  cout << "B: " << B << endl;
+
 }
 
 bool
 Pri_perceptron::adjust(double *x, int y){
   double sum = 0.0;
-  int size = sizeof(x)/sizeof(x[0]);
-  for(int i = 0; i <= size; i++){
+  for(int i = 0; i < size; i++){
     sum += w[i] * x[i];
   }
   sum += b;
   sum *= y;
-  /*cout << "x:" << x[0] << " " << x[1] << endl;
-  cout << "y:" << y << endl;
-  cout << "b:" << b << endl;
-  cout << "sum" << sum << endl;*/
   if(sum <= 0){
-    for(int i = 0; i <= size; i++){
+    for(int i = 0; i < size; i++){
       w[i] = w[i] + learningRate * y * x[i];
     }
     b += learningRate * y;
@@ -46,75 +51,140 @@ Pri_perceptron::adjust(double *x, int y){
   }
 }
 
-void
-readFile(double (*x)[3], double* y, string filename){
-  /*ifstream xStream;
-  xStream.open("ex4x.dat", ios::in);
-  ifstream yStream; 
-  yStream.open("ex4y.dat", ios::in);
-  string xtemp, xtemp2, ytemp, ytemp2;
-  int i = 0;
-  while(getline(xStream, xtemp) && getline(yStream, ytemp)){
-    istringstream xBand(xtemp);
-    istringstream yBand(ytemp);
-    for(int j = 0; j < 2; j++){
-      xBand >> xtemp2;
-      x[i][j] = atof(xtemp2.data());
+double
+Pri_perceptron::pocketAl(double (*x)[3], int *y, int iter, int setSize){
+  cout << iter << " " << setSize << endl;
+  srand((unsigned)time(NULL));
+  int iternum = 0;
+  int randomNum;
+  while(iternum++ < iter){
+    randomNum = random(0, setSize);
+    if(predict(x[randomNum], y[randomNum]) == true){
+      runw += 1;
+      if(runw > runW){
+        numokw = predictAll(x, y, setSize);
+        if(numokw > numokW){
+          for(int i; i < size; i++) W[i] = w[i];
+          B = b;
+          runW = runw;
+          numokW = numokw;
+          if(numokW == 1) return 1;
+        }
+      }
     }
-    yBand >> ytemp2;
-    y[i] = (atof(ytemp2.data()) ? 1.0 : -1.0);
-    i++;
-  }*/
+    else{
+        for(int i = 0; i < size; i++){
+          w[i] = w[i] + learningRate * y[randomNum] * x[randomNum][i];
+        }
+        b += learningRate * y[randomNum];
+        runw = 0;
+      }
+    }
+  return numokW;
+}
+
+bool
+Pri_perceptron::predict(double* x, int y){
+  int answer;
+  double sum = 0;
+  for(int i = 0; i < size; i++){
+    sum += x[i] * w[i];
+    //cout << sum << " ";
+  }
+  sum += b;
+  if(sum > 0) answer = 1;
+  else        answer = -1;
+
+  if(answer == y) return true;
+  else            return false;
+}
+
+bool
+Pri_perceptron::predictW(double* x, int y){
+  int answer;
+  double sum = 0;
+  for(int i = 0; i < size; i++){
+    sum += x[i] * W[i];
+  }
+  sum += B;
+
+  if(sum > 0) answer = 1;
+  else        answer = -1;
+
+  if(answer == y) return true;
+  else            return false;
+}
+
+int
+Pri_perceptron::predictAll(double (*x)[3], int *y, int setSize){
+  int hit = 0;
+  for(int i = 0; i < setSize; i++){
+    if(predict(x[i], y[i]) == true) hit++;
+  }
+  return hit;
+}
+
+void
+readFile(double (*x)[3], int* y, string filename, int size){
   ifstream fileStream;
   fileStream.open(filename, ios::in);
   string temp,temp2;
   int count = 0;
-
-  while(getline(fileStream,temp)){
-    cout << (string)temp << endl;
-    //istringstream xBand(temp);
-    const char* c = ",";
-    char* temp3 = strtok(temp, c);
-    /*for(int i = 0; i <= 3; i++){
+  int num = 0;
+  cout << size << endl;
+  while(num++ < size){
+    getline(fileStream,temp);
+    istringstream xBand(temp);
+    for(int i = 0; i <= 3; i++){
       xBand >> temp2;
       if(i <= 2) x[count][i] = atof(temp2.data());
       else       y[count] = atof(temp2.data());
-    }*/
-    /*x[0] = atof(temp3[0]);
-    x[1] = atof(temp3[1]);
-    x[2] = atof(temp3[2]);
-    y[0] = atof(temp3[3]);*/
-
+      if(y[count] == 1) y[count] = 1;
+      else              y[count] = -1;
+    }
     count++;
-  }
+}
 }
 int main(){
-  int num = 80;
-  double w[2] = {0.0, 0.0};
-  Pri_perceptron perceptron = Pri_perceptron(1.0, w, 0.0);
-  
-  static double inputx[259][3];
-  static double inputy[259];
-  
-  readFile(inputx, inputy, "dataset");
+  int num = 259;
+  double w[3] = {0.0, 0.0, 0.0};
 
-  /*bool judge = false;
-  while(1){
+  Pri_perceptron perceptron = Pri_perceptron(1.0, w, 0.0,3); 
+  Pri_perceptron perceptron2 = Pri_perceptron(1.0, w, 0.0, 3);
+
+  double inputx[259][3];
+  int inputy[259];
+  double predictx[47][3];
+  int predicty[47];
+  readFile(predictx, predicty, "predict", 47);
+  readFile(inputx, inputy, "dataset", num);
+
+  int iter = 0;
+  bool judge = false;
+  while(iter++ < 1000000){
     for(int i = 0; i < num; i++){
-      if(perceptron.adjust(x[i], y[i]) == true){
+      if(perceptron.adjust(inputx[i], inputy[i]) == true){
          judge = true;
-         perceptron.getAnswer();
-         cout << "The adjusted num: " << i << endl;
-    }
+      }
     }
     if(judge == true){judge = false; continue;}
     else break;
   }
-  perceptron.getAnswer();
-  return 0;*/
+  perceptron.getAnswer(); 
 
-  /*for(int i = 0; i < 259; i++){
-    cout << inputx[i][0] << " " << inputx[i][1] << " " << inputx[i][2] << 
-      " " << inputy[i] << endl;
-  }*/
+  int hit = 0;
+  for(int j = 0; j < 47; j++){
+    if(perceptron.predict(predictx[j], predicty[j])) hit++;
+  }
+  cout << (double)hit / 47 << endl;
+
+  cout << perceptron2.pocketAl(inputx, inputy, 1000000, 259) << endl;
+  perceptron2.getAnswer();
+  hit = 0;
+  for(int j = 0; j < 47; j++){
+    if(perceptron.predictW(predictx[j], predicty[j])) hit++;
+  }
+  cout << (double)hit / 47 << endl;
+  return 0;
+
 }
--- perceptron/pri_perceptron.h
+++ perceptron/pri_perceptron.h
@@ -1,20 +1,39 @@
+#include<iostream>
+using namespace std;
 #ifndef PRI_PERCEPTRON_H
 #define PRI_PERCEPTRON_H
 
 double w0[2] = {0.0,0.0};
 class Pri_perceptron{
   public:
-    Pri_perceptron(): learningRate(0.0),w(w0),b(0.0) {}
-    Pri_perceptron(double learningRate, double* w, double b):
-      learningRate(learningRate), w(w), b(b){}
+    Pri_perceptron(): learningRate(0.0),w(w0),b(0.0),size(2) {}
+    Pri_perceptron(double learningRate, double* w, double b, int size):
+      learningRate(learningRate), w(w), b(b),size(size), W(w), B(b), runw(0),runW(0),numokw(0),numokW(0){}
   
   public:
+      //get parameter of perceptron
       void getAnswer();
+      //adjust parameter
       bool adjust(double* x, int y);
+      //implement poekct algorithm
+      double pocketAl(double (*x)[3], int *y,int iter, int setSize);
+      //predict with w(primary form)
+      bool predict(double* x, int y);
+      //predict throughout dataset
+      int predictAll(double (*x)[3], int *y, int size);
+      //predict with W(pocket algorithm)
+      bool predictW(double *x, int y);
   private:
       double learningRate;
       double* w;
       double b;
+      double* W;
+      double B;
+      int runw;
+      int runW;
+      int numokw;
+      int numokW;
+      int size;
 };
 
 #endif
